30b37ae34c51d271867af6c5a9c3f9608e9d0ebb
Moustapha THIAM <moustaphathiam@hotmail.com>
2026-05-11 18:20:47 +0200

    feat: mise a jour app v4.0 - test Azure CI/CD

--- PHASES/02-conteneurs/APP/app.py
+++ PHASES/02-conteneurs/APP/app.py
@@ -4,6 +4,6 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello from KGT Cloud Formation ! v3.0 - CI/CD complet !")
+        self.wfile.write(b"Hello from KGT Cloud Formation ! v4.0 - Azure CI/CD !")
 
 HTTPServer(('', 8080), Handler).serve_forever()

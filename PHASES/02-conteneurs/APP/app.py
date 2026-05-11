11a5d155a54d7638d0f100f16f1919ebbb051012
Moustapha THIAM <moustaphathiam@hotmail.com>
2026-05-11 16:37:02 +0200

    feat: mise a jour app v2.0

--- PHASES/02-conteneurs/APP/app.py
+++ PHASES/02-conteneurs/APP/app.py
@@ -4,6 +4,6 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello from KGT Cloud Formation ! v1.0")
+        self.wfile.write(b"Hello from KGT Cloud Formation ! v2.0 - deploye par GitHub Actions !")
 
 HTTPServer(('', 8080), Handler).serve_forever()
